a458256915803c4a4e2e1118314525f5bcd60a8f
Murilojunqueira <m.junqueira@yahoo.com.br>
2019-09-12 11:45:42 -0300

    Insersão do Cap2

---
+++ Cap2/initLesson.R
@@ -0,0 +1,15 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+swirl_options(swirl_logging = TRUE)
+
+
+
+# Oscar DataBase
+Oscar <- as.data.frame(list(Age = c("20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80-89"), 
+                            f_Actress = c(27, 34, 13, 2, 4, 1, 1), 
+                            f_Actor = c(1, 26, 35, 13, 6, 1, 0)))
+
+
