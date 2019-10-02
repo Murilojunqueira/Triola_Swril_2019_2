af02161dbf0106f44d835842e96d92f0033c4971
Murilojunqueira <m.junqueira@yahoo.com.br>
2019-10-01 21:30:12 -0300

    Add Cap 3

---
+++ Cap3/initLesson.R
@@ -0,0 +1,12 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+
+
+
+IMC <- c(22.13, 27.20, 29.21, 35.40, 26.79, 20.85, 25.68, 36.38, 18.71, 19.43, 47.24, 
+         24.91, 26.60, 33.11, 28.65, 24.96, 20.31, 41.28, 20.85, 26.21, 17.62, 37.48, 
+         26.36, 44.60, 23.70, 24.22, 33.68, 20.37, 26.41, 29.02, 31.21, 29.18, 25.89, 
+         23.50, 39.98, 24.80, 27.20, 42.77, 31.30, 22.44)
